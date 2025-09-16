f2810a795ee417991a23776c0a2dbd8a854e24db
rraphael <raphael.rameix@ynov.com>
2025-09-16 14:12:03 +0200

    affiche spécification du chara

--- displayInfo.go
+++ displayInfo.go
@@ -2,11 +2,10 @@ package projetred
 
 import "fmt"
 
-func displayInfo(c1 Character) {
-	fmt.Println(c1.nom)
-	fmt.Println(c1.classe)
-	fmt.Println(c1.niveau)
-	fmt.Println(c1.PV_max)
-	fmt.Println(c1.PV)
-	fmt.Println(c1.attaque)
+func DisplayInfo(c1 Character) {
+	fmt.Println("Nom du personnage : ", c1.nom)
+	fmt.Println("Classe : ", c1.classe)
+	fmt.Println("Niveau du personnage : ", c1.niveau)
+	fmt.Println("Points de vie : ", c1.PV, "/", c1.PV_max)
+	fmt.Println("Points de dégâts : ", c1.attaque)
 }
