c42890e4b54accfc81deaee5a985729e3a0ab56f
rraphael <raphael.rameix@ynov.com>
2025-09-16 11:52:59 +0200

    utilise une potion de soin

--- takePot.go
+++ takePot.go
@@ -2,11 +2,11 @@ package projetred
 
 func takePot(c1 Character) {
 	for i := 0; i < len(c1.inventaire); i++ {
-		if c1.inventaire[i] == string("potionheal") {
+		if c1.inventaire[i] == "potion de soin" {
 			c1.PV += 50
 		}
 		if c1.PV > c1.PV_max {
-			c1.PV = 100
+			c1.PV = c1.PV_max
 		}
 	}
 }
