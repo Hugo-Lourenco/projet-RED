1ef33c60f9e6e7b0f910c30ebf3c7f2306d2da36
Hugo-Lourenco <hugo.lourenco@ynov.com>
2025-09-16 14:14:45 +0200

    classe

--- Classe.go
+++ Classe.go
@@ -2,24 +2,24 @@ package projetred
 
 func NewChevalierCharacter(nom string) Character {
     return Character{
-        nom:        nom,
-        classe:     "Chevalier",
-        niveau:     1,
+        Nom:        nom,
+        Classe:     "Chevalier",
+        Niveau:     1,
         PV_max:     100,
         PV:         50,
-        attaque:    6,
-        inventaire: []string{},
+        Attaque:    6,
+        Inventaire: []string{},
     }
 }
 
 func NewMagicienCharacter(nom string) Character {
     return Character{
-        nom:        nom,
-        classe:     "Magicien",
-        niveau:     1,
+        Nom:        nom,
+        Classe:     "Magicien",
+        Niveau:     1,
         PV_max:     90,
         PV:         40,
-        attaque:    8,
-        inventaire: []string{},
+        Attaque:    8,
+        Inventaire: []string{},
     }
 }
