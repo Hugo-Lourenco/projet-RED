06482950bfacd1ee2091f5a4d4140afaab85e9f0
Hugo-Lourenco <hugo.lourenco@ynov.com>
2025-09-16 15:22:00 +0200

    main

--- test/main.go
+++ test/main.go
@@ -2,15 +2,11 @@ package main
 
 import (
     "fmt"
-
     "projetred"
 )
 
 func main() {
     char := projetred.InitCharacter()
-    // Attention : si les champs de Character sont non-exportés (minuscules),
-    // main ne peut pas y accéder directement. Prévois des getters ou rends
-    // les champs exportés si tu veux les afficher ici.
-    _ = char
+    projetred.DisplayInfo(char)
     fmt.Println("Jeu démarré.")
 }
