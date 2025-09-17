527d95610a33674b4343b12799a927c5fecac37e
rraphael <raphael.rameix@ynov.com>
2025-09-17 15:04:57 +0200

    limite d'inventaire

--- LimiteInv.go
+++ LimiteInv.go
@@ -3,13 +3,13 @@ package projetred
 import "fmt"
 
 func CanAddItem(inventaire []string) bool {
-	return len(inventaire) < 10
+	return len(inventaire) < C1.Max_Inv
 }
 
 func AddItem(c1 *Character, item string) {
 	if CanAddItem(c1.Inventaire) {
 		c1.Inventaire = append(c1.Inventaire, item)
-		fmt.Printf("Ajouté : %s (%d/10)\n", item, len(c1.Inventaire))
+		fmt.Printf("Ajouté : %s (%d/%d)\n", item, len(c1.Inventaire), c1.Max_Inv)
 	} else {
 		fmt.Println("Ton inventaire est plein !")
 	}
