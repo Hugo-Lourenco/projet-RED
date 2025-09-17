1b8c831699db2d2b15c283721189e64455ae375c
rraphael <raphael.rameix@ynov.com>
2025-09-17 12:22:15 +0200

    menu

--- Menu.go
+++ Menu.go
@@ -12,9 +12,10 @@ func Menu() {
 
     fmt.Println("1) Information perso")
     fmt.Println("2) Inventaire")
-    fmt.Println("3) Quitter")
-    fmt.Println("4) Marchand")
-    fmt.Print("Choix 1, 2 , 3 ou 4 ? ")
+    fmt.Println("3) Marchand")
+    fmt.Println("4) Quitter")
+    fmt.Println("5) Retour")
+    fmt.Print("Choix 1, 2 , 3 , 4 ou 5 ? ")
     choice, _ := reader.ReadString('\n')
     choice = strings.TrimSpace(choice)
 
@@ -28,6 +29,8 @@ func Menu() {
             Marchand()
         case "4" : 
             os.Exit(0)
+        case "5" : 
+            break
         default:
             fmt.Println("Choix invalide.")
         continue 
