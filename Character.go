f948a957821c8e5f513ebd86a90d7e19c0d1cac4
Hugo-Lourenco <hugo.lourenco@ynov.com>
2025-09-17 10:24:47 +0200

    struc chara

--- Character.go
+++ Character.go
@@ -1,13 +1,14 @@
 package projetred
 
 type Character struct {
-	Nom	string
-	Classe	string
-	Niveau	int
-	PV_max	int
-	PV	int
-	Attaque	int
-	Or int 
+	Nom			string
+	Classe		string
+	Niveau		int
+	PV_max		int
+	PV			int
+	Attaque		int
+	Or 			int 
 	Inventaire	[]string
+	Skills 		[]Skill
 }
 var c1 Character
