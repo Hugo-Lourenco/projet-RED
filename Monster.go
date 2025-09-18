084e3000cf0b38676a1cc5136b74a59a62c704f4
rraphael <raphael.rameix@ynov.com>
2025-09-18 14:28:50 +0200

    MONSTRE MONSTRE triple MONSTRE git add Monster.go

--- Monster.go
+++ Monster.go
@@ -46,39 +46,3 @@ func Monstenemy(nom string) Monster {
 	}
 	return m
 }
-
-// pattern d'attaque du goblin tous les 3 tours il tape x2
-func goblinPattern(goblin *Monster, target *Character, turn int) {
-	var dmg int
-	if turn%3 == 0 {
-		dmg = goblin.Attaque * 2
-	} else {
-		dmg = goblin.Attaque
-	}
-	target.PV -= dmg
-	if target.PV < 0 {
-		target.PV = 0
-	}
-	fmt.Printf("%s inflige à %s %d dégâts\n", goblin.Nom, target.Nom, dmg)
-	fmt.Printf("PV de %s : %d / %d\n", target.Nom, target.PV, target.PV_max)
-}
-
-// combat tour par tour joueur vs monstre
-func trainingFight(player *Character, monster *Monster) {
-	turn := 1
-	for C1.PV > 0 && M.PV > 0 {
-		fmt.Printf("\n===== Tour %d =====\n", turn)
-		charTurn(&C1, &M)
-		if M.PV <= 0 {
-			fmt.Printf("%s est vaincu !\n", M.Nom)
-			break
-		}
-		goblinPattern(monster, player, turn)
-		if player.PV <= 0 {
-			fmt.Printf("%s est vaincu !\n", C1.Nom)
-			break
-		}
-		turn++
-	}
-	fmt.Println("Fin du combat d'entraînement.")
-}
