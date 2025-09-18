e384f114852a17864e90cec12cef811feebbf07f
Hugo-Lourenco <hugo.lourenco@ynov.com>
2025-09-18 12:19:24 +0200

    potion de soin

--- TakePot.go
+++ TakePot.go
@@ -3,7 +3,7 @@ package projetred
 func takePot(c1 Character) {
 	for i := 0; i < len(c1.Inventaire); i++ {
 		if c1.Inventaire[i] == "Potion de soin" {
-			c1.PV += 50
+			c1.PV += 30
 			c1.Inventaire = append(c1.Inventaire[:i], c1.Inventaire[i+1:]...)
 			if c1.PV > c1.PV_max {
 				c1.PV = c1.PV_max
