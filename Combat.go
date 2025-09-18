bba390283a4e8056583eb4bb9029851823260458
Hugo-Lourenco <hugo.lourenco@ynov.com>
2025-09-18 12:20:44 +0200

    Refactor combat functions to use player and monster

--- Combat.go
+++ Combat.go
@@ -8,33 +8,33 @@ import (
 )
 
 // poison sur le monstre
-func poisonPotMonster(M *Monster) {
+func poisonPotMonster(m *Monster) {
     for i := 0; i < 3; i++ {
-        M.PV -= 10
-        if M.PV < 0 {
-            M.PV = 0
+        m.PV -= 10
+        if m.PV < 0 {
+            m.PV = 0
         }
-        fmt.Printf("Effet du poison sur le monstre : %d/%d PV\n", M.PV, M.PV_max)
+        fmt.Printf("Effet du poison sur le monstre : %d/%d PV\n", m.PV, m.PV_max)
     }
 }
 
 // Combat 
-func Combat(C1 *Character, M *Monster) {
+func Combat(player *Character, monster *Monster) {
     reader := bufio.NewReader(os.Stdin)
     turn := 1
     skillAvailable := true
 
-    for C1.PV > 0 && M.PV > 0 {
+    for player.PV > 0 && monster.PV > 0 {
         fmt.Printf("\n===== Tour %d =====\n", turn)
-        fmt.Printf("%s : %d/%d PV\n", C1.Nom, C1.PV, C1.PV_max)
-        fmt.Printf("%s : %d/%d PV\n", C1.Nom, C1.PV, C1.PV_max)
+        fmt.Printf("%s : %d/%d PV\n", player.Nom, player.PV, player.PV_max)
+        fmt.Printf("%s : %d/%d PV\n", monster.Nom, monster.PV, monster.PV_max)
 
         // au tour du joueur
         for {
             fmt.Println("Menu :")
             fmt.Println("1) Attaquer")
             fmt.Println("2) Inventaire")
-            if skillAvailable && len(C1.Skills) > 0 {
+            if skillAvailable && len(player.Skills) > 0 {
                 fmt.Println("3) Utiliser Skill")
             }
             fmt.Print("Choix ? ")
@@ -43,21 +43,21 @@ func Combat(C1 *Character, M *Monster) {
 
             if choix == "1" {
                 // attaque 
-                dmg := C1.Attaque
-                C1.PV -= dmg
-                if C1.PV < 0 {
-                    C1.PV = 0
+                dmg := player.Attaque
+                monster.PV -= dmg
+                if monster.PV < 0 {
+                    monster.PV = 0
                 }
-                fmt.Printf("%s inflige %d dégâts à %s\n", C1.Nom, dmg, M.Nom)
+                fmt.Printf("%s inflige %d dégâts à %s\n", player.Nom, dmg, monster.Nom)
                 break
             } else if choix == "2" {
                 // inventaire
-                if len(C1.Inventaire) == 0 {
+                if len(player.Inventaire) == 0 {
                     fmt.Println("Inventaire vide.")
                     continue
                 }
                 fmt.Println("Inventaire :")
-                for i, item := range C1.Inventaire {
+                for i, item := range player.Inventaire {
                     fmt.Printf("%d) %s\n", i+1, item)
                 }
                 fmt.Print("Choisissez un objet à utiliser (numéro) ou 0 pour annuler : ")
@@ -69,14 +69,21 @@ func Combat(C1 *Character, M *Monster) {
                 var idx int
                 fmt.Sscanf(itemChoice, "%d", &idx)
                 idx--
-                if idx >= 0 && idx < len(C1.Inventaire) {
-                    item := C1.Inventaire[idx]
+                if idx >= 0 && idx < len(player.Inventaire) {
+                    item := player.Inventaire[idx]
                     if item == "Potion de soin" {
-                        takePot(*C1)
+                        heal := 30
+                        player.PV += heal
+                        if player.PV > player.PV_max {
+                            player.PV = player.PV_max
+                        }
+                        fmt.Printf("Vous utilisez %s et récupérez %d PV !\n", item, heal)
+                        player.Inventaire = append(player.Inventaire[:idx], player.Inventaire[idx+1:]...)
+                        break
                     } else if item == "Potion de poison" {
-                        poisonPotMonster(M)      
-                        fmt.Println("Vous utilisez la potion sur le monstre")     
-                        C1.Inventaire =  append(C1.Inventaire[:idx], C1.Inventaire[idx+1:]...)
+                        poisonPotMonster(monster)
+                        fmt.Println("Vous utilisez la potion de poison sur le monstre !")
+                        player.Inventaire = append(player.Inventaire[:idx], player.Inventaire[idx+1:]...)
                         break
                     } else {
                         fmt.Println("Objet non utilisable en combat.")
@@ -86,8 +93,8 @@ func Combat(C1 *Character, M *Monster) {
                     fmt.Println("Choix invalide.")
                     continue
                 }
-            } else if choix == "3" && skillAvailable && len(C1.Skills) > 0 {
-                msg := C1.Skills[0].Use(C1, M)
+            } else if choix == "3" && skillAvailable && len(player.Skills) > 0 {
+                msg := player.Skills[0].Use(player, monster)
                 fmt.Println(msg)
                 skillAvailable = false
                 break
@@ -96,24 +103,24 @@ func Combat(C1 *Character, M *Monster) {
             }
         }
 
-        if M.PV <= 0 {
-            fmt.Printf("%s est vaincu !\n", M.Nom)
+        if monster.PV <= 0 {
+            fmt.Printf("%s est vaincu !\n", monster.Nom)
             break
         }
 
         // au tour du monstre
         var dmg int
         if turn%3 == 0 {
-            dmg = M.Attaque * 2
+            dmg = monster.Attaque * 2
         } else {
-            dmg = M.Attaque
+            dmg = monster.Attaque
         }
-        C1.PV -= dmg
-        if C1.PV < 0 {
-            C1.PV = 0
+        player.PV -= dmg
+        if player.PV < 0 {
+            player.PV = 0
         }
-        fmt.Printf("%s inflige à %s %d dégâts\n", M.Nom, C1.Nom, dmg)
-        fmt.Printf("PV de %s : %d / %d\n", C1.Nom, C1.PV, C1.PV_max)
+        fmt.Printf("%s inflige à %s %d dégâts\n", monster.Nom, player.Nom, dmg)
+        fmt.Printf("PV de %s : %d / %d\n", player.Nom, player.PV, player.PV_max)
 
         turn++
         if turn%2 == 0 {
@@ -122,9 +129,11 @@ func Combat(C1 *Character, M *Monster) {
     }
 
     fmt.Println("Fin du combat.")
-    C1.PV = C1.PV_max / 2
-    if C1.PV < 1 {
-        C1.PV = 1
+    // réssussiter avec 50% de la vie
+    player.PV = player.PV_max / 2
+    if player.PV < 1 {
+        player.PV = 1
     }
-    fmt.Printf("%s récupère 50%% de ses PV : %d/%d\n", C1.Nom, C1.PV, C1.PV_max)
+    fmt.Printf("%s récupère 50%% de ses PV : %d/%d\n", player.Nom, player.PV, player.PV_max)
+    // retour au menu 
 }
