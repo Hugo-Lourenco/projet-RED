862c1a636fb1d56687ada16be98d0a27077bb451
Hugo-Lourenco <hugo.lourenco@ynov.com>
2025-09-18 10:17:50 +0200

    la mort

--- IsDead.go
+++ IsDead.go
@@ -3,18 +3,18 @@ package projetred
 import "fmt"
 
 func IsDead(c *Character) bool {
-    if c.PV <= 0 {
-        fmt.Println("Vous êtes mort !")
-        if c.PV_max > 0 {
-            c.PV = c.PV_max / 2
-            if c.PV < 1 {
-                c.PV = 1
-            }
-            fmt.Printf("Vous êtes ressuscité avec %d/%d PV\n", c.PV, c.PV_max)
-        } else {
-            fmt.Println("Impossible de ressusciter : PV_max = 0")
-        }
-        return true
-    }
-    return false
+	if c.PV <= 0 {
+		fmt.Println("Vous êtes mort !")
+		if c.PV_max > 0 {
+			c.PV = c.PV_max / 2
+			if c.PV < 1 {
+				c.PV = 1
+			}
+			fmt.Printf("Vous êtes ressuscité avec 50%% des PV : %d/%d PV\n", c.PV, c.PV_max)
+		} else {
+			fmt.Println("Impossible de ressusciter : PV_max = 0")
+		}
+		return true
+	}
+	return false
 }
