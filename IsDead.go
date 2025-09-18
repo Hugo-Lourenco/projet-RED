8090c35f36790bf14fe52cdbb3e1d56c311e3e2e
rraphael <raphael.rameix@ynov.com>
2025-09-18 14:16:48 +0200

    vérifie si le joueur est vivant

--- IsDead.go
+++ IsDead.go
@@ -2,18 +2,11 @@ package projetred
 
 import "fmt"
 
-func IsDead(c *Character) bool {
-	if c.PV <= 0 {
+func IsDead(C1 *Character) bool {
+	if C1.PV <= 0 {
 		fmt.Println("Vous êtes mort !")
-		if c.PV_max > 0 {
-			c.PV = c.PV_max / 2
-			if c.PV < 1 {
-				c.PV = 1
-			}
-			fmt.Printf("Vous êtes ressuscité avec 50%% des PV : %d/%d PV\n", c.PV, c.PV_max)
-		} else {
-			fmt.Println("Impossible de ressusciter : PV_max = 0")
-		}
+		C1.PV = C1.PV_max / 2
+		fmt.Printf("Vous êtes ressuscité avec 50%% des PV : %d/%d PV\n", C1.PV, C1.PV_max)
 		return true
 	}
 	return false
