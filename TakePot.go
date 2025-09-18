06483dceb6b810a0b248dea31651432b09916a8a
rraphael <raphael.rameix@ynov.com>
2025-09-18 16:13:13 +0200

    potion de soin

--- TakePot.go
+++ TakePot.go
@@ -1,14 +1,17 @@
 package projetred
 
-func takePot(C1 Character) {
-	for i := 0; i < len(C1.Inventaire); i++ {
-		if C1.Inventaire[i] == "Potion de soin" {
+func takePot(C1 *Character) {
+	// Vérifie que le joueur possède un potion
+	for i, item := range C1.Inventaire {
+		if item == "Potion de soin" {
+			// Ajoute les PV
 			C1.PV += 30
-			C1.Inventaire = append(C1.Inventaire[:i], C1.Inventaire[i+1:]...)
 			if C1.PV > C1.PV_max {
 				C1.PV = C1.PV_max
 			}
-			break 
+			// Supprime la potion de soin de l'inventaire
+			C1.Inventaire = append(C1.Inventaire[:i], C1.Inventaire[i+1:]...)
+			break
 		}
 	}
 }
