a0adf256afc6e86838db5e782960d29ada8e20bb
rraphael <raphael.rameix@ynov.com>
2025-09-18 09:49:44 +0200

    inventaire

--- AccessInventory.go
+++ AccessInventory.go
@@ -3,5 +3,14 @@ package projetred
 import "fmt"
 
 func accessInventory(c1 Character) {
-	fmt.Println(c1.Inventaire)
+	fmt.Println("════════════════════════ Inventaire ════════════════════════")
+	if len(c1.Inventaire) == 0 {
+        fmt.Println("                      	  [ Vide ]                 	     ")
+        fmt.Println("════════════════════════════════════════════════════════════")
+        return
+	}
+	for i, item := range c1.Inventaire {
+        fmt.Printf(" %2d. %-45s \n", i+1, item)
+	}
+	fmt.Printf("════════════════════ %d / %d emplacements ═══════════════════\n", len(c1.Inventaire), c1.Max_Inv)
 }
